0180818794bc2b5df17fffb0ccf50604e3e10fa2
Rafeef Thamer <rafeef1323@gmail.com>
2024-05-14 19:26:29 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+let num1 = 62 / 2;
+let num2 = 62 / num1;
+let multiply = num1 * num2;
+
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+
+let num3 = 20;
+let num4 = 16; 
+let mod = num3 % num4;
+
+
+let numberSet = [10, 15, 20, 5, 8];
+let max = Math.max(...numberSet);
